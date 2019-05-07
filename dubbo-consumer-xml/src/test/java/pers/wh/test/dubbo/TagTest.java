0f474a0061adde8d6d5f101924154d7684d1ab75
wanghui <wanghui@auto-fis.com>
2019-05-07 14:49:45 +0800

    使用Tag，可以用来发送到指定标签的服务上

---
+++ dubbo-consumer-xml/src/test/java/pers/wh/test/dubbo/TagTest.java
@@ -0,0 +1,25 @@
+package pers.wh.test.dubbo;
+
+import org.apache.dubbo.common.Constants;
+import org.apache.dubbo.rpc.RpcContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author wanghui
+ * @date 2019/5/7 13:15
+ */
+public class TagTest {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"consumer.xml"});
+        context.start();
+
+        //设置使用的哪个标签服务
+        RpcContext.getContext().setAttachment(Constants.TAG_KEY, "Ni1901");
+
+        HelloService helloService = (HelloService) context.getBean("helloService");
+        String s = helloService.sayHello("1");
+        System.out.println("-------------------->"+s);
+    }
+
+}
